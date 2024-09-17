88e77c43d674acdd37c699139659613317ef402b
EkramShaker <Ikramshaker@Gmail.com>
2024-09-16 23:55:40 -0400

    Update ArticleCard.jsx

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -7,7 +7,7 @@ function ArticleCard({ title, date, image, description }) {
             <h2>{title}</h2>
             <img src={image} alt={title} />  {/* Corrected this line */}
             <p className="description">{description}</p>
-            <a href="#">Continue reading...</a>
+            <a href="javascript:void(0)">Continue reading...</a>
         </article>
     );
 }
